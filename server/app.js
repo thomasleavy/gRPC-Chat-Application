c0948758a00ce4da5cc50548c5caf3bec98c506c
Thomas Leavy <thomasleavy95@live.ie>
2024-11-13 14:12:49 +0000

    Initial commit of gRPC Chat Application

--- server/app.js
+++ server/app.js
@@ -1,14 +1,45 @@
-var grpc = require("@grpc/grpc-js")
-var protoLoader = require("@grpc/proto-loader")
-var PROTO_PATH = __dirname + "/protos/proto.proto"
+var grpc = require("@grpc/grpc-js");
+var protoLoader = require("@grpc/proto-loader");
+var PROTO_PATH = __dirname + "/protos/chat.proto";
 var packageDefinition = protoLoader.loadSync(
   PROTO_PATH
-)
-var proto = grpc.loadPackageDefinition(packageDefinition).package_name
+);
+var chat_proto = grpc.loadPackageDefinition(packageDefinition).chat;
 
+var clients = {
+
+}
+
+function sendMessage(call) {
+  call.on('data', function(chat_message) {
+    if (!(chat_message.name in clients)) {
+      clients[chat_message.name] = {
+        name: chat_message.name,
+        call: call
+      };
+      
+    }
+    for (var client in clients) {
+      clients[client].call.write(
+        {
+          name: chat_message.name,
+          message: chat_message.message
+        }
+      )
+    }
+  });
+  call.on('end', function(e) {
+    console.log(e)
+  }); 
+}
 
 
 var server = new grpc.Server()
+
+server.addService(chat_proto.ChatService.service, {
+  sendMessage:sendMessage
+});
+
 server.bindAsync("0.0.0.0:40000", grpc.ServerCredentials.createInsecure(), function() {
   server.start()
 })
